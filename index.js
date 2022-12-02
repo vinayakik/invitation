02f1abc6dd0ab52151436f04fc16a817746f676f
vinayakik <vinayakkadlennavar@gmail.com>
2022-12-02 17:29:22 +0530

    added node

---
+++ index.js
@@ -0,0 +1,14 @@
+const express=require('express');
+const app=express();
+app.use(express.static(__dirname));
+app.use(express.static("public"));
+
+const port =3000;
+
+app.get("/",function(req,res){
+    // res.sendFile(__dirname+"/index.html")
+})
+
+app.listen(port, function(){      //our local server at port 3000
+    console.log("Server started on port 3000...");
+});
